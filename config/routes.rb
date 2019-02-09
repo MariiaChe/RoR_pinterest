12492a78ad31d3543053a1032c4cbd7d89a35388
Mariia <maria.bookvoed@gmail.com>
2019-02-09 12:26:46 -0300

    add:changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :pins
   get 'welcome/index'
   root 'welcome#index'
+  get 'pages/home'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
