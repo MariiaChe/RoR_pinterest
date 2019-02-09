e99d6f0903b89f671fbdab57a6e57410a2343c66
Mariia <maria.bookvoed@gmail.com>
2019-02-09 10:07:49 -0300

    add:changes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except:[ :index, :show ] 
   # GET /pins
   # GET /pins.json
   def index
@@ -19,12 +19,13 @@ class PinsController < ApplicationController
 
   # GET /pins/1/edit
   def edit
-  end
+  end   
 
   # POST /pins
   # POST /pins.json
   def create
     @pin = Pin.new(pin_params)
+    @pin.user_id = current_user.id
 
     respond_to do |format|
       if @pin.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_042245) do
+ActiveRecord::Schema.define(version: 2019_02_09_130138) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_02_02_042245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_02_02_042245) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
