ed7ca87491dcddc881e33f203126e82c222e2650
Mariia <maria.bookvoed@gmail.com>
2019-02-16 09:08:57 -0300

    add:bootstrap4,footer, vista pinsof y mypins

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   get 'pages/home'
+  get 'mypins' => 'pins#mypins'
+  get 'pinsof/:user_id' => 'pins#pinsof', :as => 'pinsof'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
